a8f1aa20f586ef676cc4d7fb73da5d49cf85b15f
Alexander <alexander5243188@yahoo.es>
2025-06-20 20:48:28 -0400

    Uso de arreglos

---
+++ 03_arreglos.php
@@ -0,0 +1,41 @@
+<?php
+// Declaración de variables
+$curso = "Curso de PHP";
+$precio = "100 Bs";
+$fecha = "2025-05-29";
+$archivado = true;
+$inscrito = 10;
+$nivel = "Intermedio"; // Básico, Intermedio, Avanzado
+$tags = ["PHP", "Programación", "Backend", "Desarrollo Web", "JavaScript", "HTML", "CSS"];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $curso?></title>
+</head>
+<body>
+    <p>Introducción al <?= $curso?></p>
+    <p>Tiene un precio de <?= $precio?> y el lanzamiento es <?= $fecha?> </p>
+    <p>Estado del curso: <?php if($archivado){
+        echo "<p>Activo</p>";
+    } else {
+        echo "<p>Archivado</p>";
+    }
+    ?></p>
+    <p><?= $archivado ? "<p>Alumnos inscritos $inscrito</p>" : "<p></p>" ?></p>
+    <p><?= $nivel?></p>
+    <p><?php 
+    if($nivel== "Básico"){
+        echo "<p>Recomendado para principitantes en $tags[1], $tags[0] en $tags[2] y $tags[3] </p>";
+        } else if( $nivel =="Intermedio"){
+            echo "<p>Recomendado para estudiantes con conocimientos basicos de $tags[1] en $tags[0], $tags[4], $tags[5], $tags[6]</p>"; 
+        }
+            else if( $nivel == "Avanzado"){ 
+                echo "<p>Curso ideal para estudiantes con conocimientos solidos de programación</p>";} 
+        ?>
+    </p>
+</body>
+</html>
